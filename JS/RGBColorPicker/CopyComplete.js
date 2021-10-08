91fd492cd17bb07e203655bf10bf5b4afc556383
OctSkyOut <kms3335k@naver.com>
2021-10-08 19:41:20 +0900

    js import export오류로 인한 모듈 해제

--- JS/RGBColorPicker/CopyComplete.js
+++
@@ -1,8 +0,0 @@
-const copyMsg = document.querySelector(".copy-msg");
-
-const copyComplete = () => {
-	copyMsg.classList.add("appear-msg");
-	setTimeout(() => copyMsg.classList.remove("appear-msg"), 1000);
-};
-
-export default copyComplete;
